2d915775954fa003c8a72949daf7ba1ca41f4550
Madison Hicks <madison.carlisle.hicks@gmail.com>
2017-06-09 13:25:53 -0500

    add gitignore, index.html

--- app/views.py
+++ app/views.py
@@ -1,16 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname' : 'jane'}
-    return '''
-    <html>
-        <head>
-            <title>Project: Blog</title>
-        </head>
-        <body>
-            <h1>Hello ''' + user['nickname'] + '''</h1>
-        </body>
-    </html>
-    '''
+    return render_template('index.html',
+                           title='home',
+                           user=user)
